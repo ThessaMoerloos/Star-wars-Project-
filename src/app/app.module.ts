a9412d766337e71730df8c654a4e05d93e912202
Thessa Moerloos <thessa.moerloos@student.kdg.be>
2017-08-25 20:03:43 +0200

    Toevoegen nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,29 +8,22 @@ import { AppRoutingModule }  from './app.routing.module';
 import { HttpModule, JsonpModule } from '@angular/http';
 
 
-/*import { FilmListComponent } from './Film/film-list.component';*/
 import { PeopleListComponent } from './People/people-list.component';
 import { PlanetListComponent } from './Planet/planet-list.component';
 import { SpeciesListComponent } from './Species/species-list.component';
-/*import { StarshipListComponent } from './Starship/starship-list.component';*/
-/*import { VehicleListComponent } from './Vehicle/vehicle-list.component';*/
 
-/*import { FilmService } from './Film/film-list.service';*/
+
 import { PlanetService } from './Planet/planet-list.service';
 import { PeopleService } from "./People/people-list.service";
 import { SpeciesService } from "./Species/species-list.service";
-/*import { StarshipService } from "./Starship/starship-list.service";
-import { VehicleService } from "./Vehicle/vehicle-list.service";*/
+
 
 @NgModule({
   declarations: [
     AppComponent,
-  /*FilmListComponent,*/
     PeopleListComponent,
     PlanetListComponent,
     SpeciesListComponent
-    /*StarshipListComponent,
-    VehicleListComponent*/
   ],
   imports: [
     BrowserModule,
@@ -39,12 +32,10 @@ import { VehicleService } from "./Vehicle/vehicle-list.service";*/
     FormsModule
   ],
   providers: [
-    /*FilmService,*/
     PeopleService,
     PlanetService,
     SpeciesService
-    /*StarshipService,
-    VehicleService*/
+
   ],
   bootstrap: [AppComponent]
 })
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,21 +2,18 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-/*import { FilmListComponent } from './Film/film-list.component';*/
+
 import { PeopleListComponent } from './People/people-list.component';
 import { PlanetListComponent } from './Planet/planet-list.component';
 import { SpeciesListComponent } from './Species/species-list.component';
-/*import { StarshipListComponent } from './Starship/starship-list.component';
-import { VehicleListComponent } from './Vehicle/vehicle-list.component';*/
+
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'people', pathMatch: 'full' },
-/*  { path: 'film',  component: FilmListComponent},*/
   { path: 'planet', component: PlanetListComponent },
   { path: 'people', component: PeopleListComponent },
   { path: 'species', component: SpeciesListComponent }
-  /*{ path: 'starship', component: StarshipListComponent },
-  { path: 'vehicle', component: VehicleListComponent }*/
+
 ];
 
 @NgModule({
