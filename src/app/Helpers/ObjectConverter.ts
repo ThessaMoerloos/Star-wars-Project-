1247056b07103f935f100fc831fb75605594b252
Thessa Moerloos <thessa.moerloos@student.kdg.be>
2017-08-25 22:04:30 +0200

    Verwijderen overbodige convertors

--- src/app/Helpers/ObjectConverter.ts
+++ src/app/Helpers/ObjectConverter.ts
@@ -1,26 +1,11 @@
-import { Film }  from '../Film/Film';
+
 import { People }  from '../People/People';
 import { Planet }  from '../Planet/Planet';
 import { Species }  from '../Species/Species';
-import { Starship }  from '../Starship/Starship'
-import { Vehicle }  from '../Vehicle/Vehicle';
+
 
 
 export class ObjectConverter {
-
-  public convertResponseToFilm(r:any): Film{
-
-    let film = <Film>({
-      title: r.title,
-      episode_id: r.episode_id,
-      opening_crawl: r.opening_crawl,
-      director: r.director,
-      producer: r.producer,
-      release_date: r.release_date,
-    });
-
-    return film;
-  };
 
 
   public convertResponseToPeople(r:any): People{
@@ -80,47 +65,7 @@ export class ObjectConverter {
     return species;
   };
 
-  public convertResponseToStarship(r:any): Starship{
 
-    let starship = <Starship>({
-      MGLT: r.MGLT,
-      cargo_capacity: r.cargo_capacity,
-      openingCrawl: r.openingCrawl,
-      consumables: r.consumables,
-      created: r.created,
-      crew: r.crew,
-      edited: r.edited,
-      hyperdrive_rating: r.hyperdrive_rating,
-      length: r.length,
-      manufacturer: r.manufacturer,
-      max_atmosphering_speed: r.max_atmosphering_speed,
-      model: r.model,
-      name: r.name,
-      passengers: r.passengers,
-      starship_class: r.starship_class
-    });
-
-    return starship;
-  }
-
-  public convertResponseToVehicle(r:any): Vehicle{
-
-    let vehicle = <Vehicle>({
-      name: r.name,
-      model: r.model,
-      manufacturer: r.manufacturer,
-      cost_in_credits: r.cost_in_credits,
-      length: r.length,
-      max_atmosphering_speed: r.max_atmosphering_speed,
-      crew: r.crew,
-      passengers: r.passengers,
-      cargo_capacity: r.cargo_capacity,
-      consumables: r.consumables,
-      vehicle_class: r.vehicle_class
-    });
-
-    return vehicle;
-  }
 
 
 }
